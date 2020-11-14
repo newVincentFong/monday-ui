b23f3b5deafde75d3eb78359479734ad86687ad1
vincentfong <591216746@qq.com>
2020-11-14 23:59:23 +0800

    📦 add menu for MenuButton

--- src/components/MenuButton/index.tsx
+++ src/components/MenuButton/index.tsx
@@ -3,9 +3,13 @@ import './index.less'
 import classNames from 'classnames'
 import { CSS_PREFIX } from '../../util/constants'
 import DropdownChevronDown from 'monday-ui-react-core/dist/icons/DropdownChevronDown'
+import Settings from 'monday-ui-react-core/dist/icons/Settings'
+import Sort from 'monday-ui-react-core/dist/icons/Sort'
+import Info from 'monday-ui-react-core/dist/icons/Info'
 
 const COMPONENT_NAME = 'menu-button'
 const PREFIX = CSS_PREFIX + COMPONENT_NAME
+const MENU_PREFIX = CSS_PREFIX + 'menu'
 
 interface MenuButtonProps {
     size: 'lg' | 'md' | 'smd' | 'sm' | 'xs' | undefined
@@ -13,11 +17,42 @@ interface MenuButtonProps {
 
 export const MenuButton: FC<MenuButtonProps> = ({size = 'xs'}) => {
     const [isOpen, setIsOpen] = useState(false)
+    const SIZE = 16
 
     return (
         <div className={`${PREFIX}-component`} onClick={() => {setIsOpen(!isOpen)}}>
             <div className={classNames(`${PREFIX}`, `${PREFIX}-${size}`, isOpen ? 'open' : '')}>
                 <DropdownChevronDown />
+                {
+                    !isOpen ? '' : (
+                        <div className={classNames(`${MENU_PREFIX}-dialog`, `${MENU_PREFIX}-md`)}>
+                            <div className={`${MENU_PREFIX}-inner`}>
+                                <div className={`${MENU_PREFIX}-section`}>
+                                    <div className={`${MENU_PREFIX}-item`}>
+                                        <div className={`${MENU_PREFIX}-icon`}>
+                                            <Settings size={SIZE} />
+                                        </div>
+                                        <div className={`${MENU_PREFIX}-title`}>Column Setting</div>
+                                    </div>
+                                    <div className={`${MENU_PREFIX}-item`}>
+                                        <div className={`${MENU_PREFIX}-icon`}>
+                                            <Sort size={SIZE} />
+                                        </div>
+                                        <div className={`${MENU_PREFIX}-title`}>Sort Column</div>
+                                    </div>
+                                </div>
+                                <div className={`${MENU_PREFIX}-section`}>
+                                    <div className={`${MENU_PREFIX}-item`}>
+                                        <div className={`${MENU_PREFIX}-icon`}>
+                                            <Info size={SIZE} />
+                                        </div>
+                                        <div className={`${MENU_PREFIX}-title`}>Add Description</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
