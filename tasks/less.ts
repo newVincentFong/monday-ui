9da252d6c0a3e7f01ff1ee671058e0a3a60c5a5f
vincentfong <591216746@qq.com>
2020-12-20 23:18:26 +0800

    🔧  fix less @import's relative paths resolving

--- tasks/less.ts
+++ tasks/less.ts
@@ -13,16 +13,20 @@ export function lessTask(options: LessTaskOptions): TaskFunction {
     return function lessTaskFunction(done: (err?: Error) => void) {
         const files = glob.sync(path.resolve(process.cwd(), 'src/**/*.less'))
         if (files.length) {
+            logger.info(`detect ${files.length} less files`)
             const tasks = files.map(fileName => function(cb: any): void {
                 fileName = path.resolve(fileName)
                 const lessInput = fs.readFileSync(fileName, { encoding: 'utf8' })
-                less.render(lessInput).then(output => {
+                less.render(lessInput, {
+                    paths: [path.dirname(fileName)]
+                }).then(output => {
                     fs.writeFileSync(fileName.replace('src/components', options.output).replace('.less', '.css'), output.css)
                 }).catch(err => console.error(err))
             })
 
             parallelLimit(tasks, 5, done)
         } else {
+            logger.warn('detect no less files')
             done()
         }
     }
